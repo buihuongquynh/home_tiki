985b21b95970079c86d2ddba29291716d9d87569
buihuongquynh <buigiangquynh@gmail.com>
2021-02-27 18:14:58 +0700

    d

---
+++ tiki/assets/js/main.js
@@ -0,0 +1,55 @@
+$('.sale .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+$('.toDay .owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 5
+        },
+        600: {
+            items: 5
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+   
